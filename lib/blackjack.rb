3231034c2e8e8b9dacdfeb1dcd42183c0f487080
Austin Buhler <austinbuhler@gmail.com>
2020-05-04 20:55:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,9 +54,9 @@ end
 def runner
   welcome
   card_total = initial_round
-  until card_total >= 21
+  until card_total > 21
     card_total = hit?(card_total)
-    user_input = get_user_input
+    display_card_total(card_total)
   end
   end_game(card_total)
 end
