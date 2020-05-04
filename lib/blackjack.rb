6bc471996a789351972a60088657b7401475b1fd
Austin Buhler <austinbuhler@gmail.com>
2020-05-04 20:45:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,5 +53,10 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  until card_total >= 21 
+    hit?(card_total)
+  end
+  end_game(card_total)
 end
